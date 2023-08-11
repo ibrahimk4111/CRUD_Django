a6f382ddf9d646cb5848a34cdf5234658fd17ce8
ibrahimk4111 <ibrahimk4111@gmail.com>
2023-08-12 01:13:09 +0600

    whitenoise

--- Todo_Backend/settings.py
+++ Todo_Backend/settings.py
@@ -128,18 +128,11 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'todo_reactjs_app/build/static'
 ]
-# STATIC_ROOT = os.path.join (BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join (BASE_DIR, 'staticfiles_build', 'static')
 
-# Following settings only make sense on production and may break development environments.
-if DEBUG:
-    # Tell Django to copy statics to the `staticfiles` directory
-    # in your application directory on Render.
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-    # Turn on WhiteNoise storage backend that takes care of compressing static files
-    # and creating unique names for each version so they can safely be cached forever.
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
+# Turn on WhiteNoise storage backend that takes care of compressing static files
+# and creating unique names for each version so they can safely be cached forever.
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
