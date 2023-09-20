d1d3fa346225629604ca5c5bea677eb21867e75c
ibrahimk4111 <ibrahimk4111@gmail.com>
2023-09-20 22:45:34 +0600

    isDone added

--- noteApi/models.py
+++ noteApi/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Note(models.Model):
     body = models.TextField()
-    isDone = models.BooleanField(default=False)
+    is_complete = models.BooleanField(default=False)
     updated = models.DateField(auto_now=True)
     created = models.DateField(auto_now_add=True)
     
