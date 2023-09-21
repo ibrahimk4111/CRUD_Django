96b7928964e0e24538e927e57ab04dadc9c675c4
ibrahimk4111 <ibrahimk4111@gmail.com>
2023-09-21 23:46:30 +0600

    db

--- noteApi/models.py
+++ noteApi/models.py
@@ -2,5 +2,6 @@ from django.db import models
 
 class dbNote(models.Model):
     body = models.TextField()
+    isDone = models.BooleanField()
     updated = models.DateField(auto_now=True)
     created = models.DateField(auto_now_add=True)
